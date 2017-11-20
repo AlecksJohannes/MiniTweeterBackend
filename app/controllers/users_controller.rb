4d70d743a915c891f7446b5e30812a733b237c47
AlecksJohannes <alecksjohanssen@gmail.com>
2017-11-20 22:22:33 +0700

    Pushed to HK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def create
+    @user = User.where(email: params[:email]).first_or_create
+    if @user.persisted?
+      render json: { status: true, user: @user.as_json }
+    else
+      render json: { status: false }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  resources :users
+
   resources :tweets do 
-    resources :re_tweets
+    resources :retweets
   end
 end
